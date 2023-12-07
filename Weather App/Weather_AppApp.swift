3e73a74cd6267587d25a2b701cde31727e75efb5
Muhammad Hammad <hammadarshad834@gmail.com>
2023-12-06 22:13:40 -0500

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Muhammad Hammad on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
